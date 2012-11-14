d94838aaca175015428253411ba85d233ce92436
Julius Volz <julius@soundcloud.com>
2012-11-14 12:50:27 +0100

    Allow configuring a redirect to a canonical host.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_filter :redirect_to_canonical_host
+
   protect_from_forgery
   include SessionsHelper
+
+  def redirect_to_canonical_host
+    canonical_host = APP_CONFIG['canonical_host']
+    if canonical_host && request.host != canonical_host
+      redirect_to "#{request.protocol}#{canonical_host}:#{request.port}#{request.fullpath}"
+    end
+  end
 end
