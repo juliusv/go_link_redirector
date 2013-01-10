66a916be1fb08f4358ba1504b992f98675f7dc0a
Julius Volz <julius@soundcloud.com>
2013-01-10 18:21:39 +0100

    Explicitly disable catchall-redirect route for reserved endpoints.

---
+++ config/initializers/reserved_endpoints.rb
@@ -0,0 +1,2 @@
+# This needs to live here for now because it's used both from routes.rb as well as the Link model.
+RESERVED_SHORT_NAMES = %w{links signin signout auth about home contact assets public int}
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
+class ReservedEndpointConstraint
+  def matches?(request)
+    RESERVED_SHORT_NAMES.each do |ep|
+      if request.path.start_with?("/#{ep}/")
+        return false
+      end
+    end
+    true
+  end
+end
+
 Goto::Application.routes.draw do
   match '/auth/google_oauth2/callback', to: 'sessions#create'
   match '/signin', to: 'sessions#new'
@@ -5,5 +16,5 @@ Goto::Application.routes.draw do
 
   resources :links
   root :to => 'links#index'
-  match '*link' => 'links#redirect'
+  match '*link' => 'links#redirect', :constraints => ReservedEndpointConstraint.new
 end
